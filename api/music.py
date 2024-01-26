ca10c195d693432ac2e8d962b44803599a3d2197
nathan21hz <nathan21hz@gmail.com>
2024-01-26 18:58:04 +0800

    get music play url from netease and kugou

--- api/music.py
+++ api/music.py
@@ -1,4 +1,5 @@
 from bson import ObjectId
+import requests
 
 from fastapi import HTTPException
 
@@ -85,6 +86,50 @@ def get_music_list_by_filter(q, album, solo, platform, language, page, size):
     cursor, count = get_music_list_by_query(query, page, size)
     return [MusicInfoModel(id=str(i["_id"]),**i) for i in cursor], count
 
+def get_netease_detail(netease_id):
+    cover_url = ""
+    play_url  = ""
+    lyric     = ""
+    # Play URL
+    play_url  = "https://music.163.com/song/media/outer/url?id={}".format(netease_id)
+    # Cover and Lyric
+    try:
+        img_req = requests.get('http://music.163.com/api/song/detail/?id={}&ids=%5B{}%5D'.format(netease_id, netease_id), timeout=5)
+        img_json = img_req.json()
+        cover_url = img_json["songs"][0]["album"]["picUrl"]+"?param=300y300"
+
+        lrc_req = requests.get('http://music.163.com/api/song/media?id={}'.format(netease_id), timeout=5)
+        lrc_json = lrc_req.json()
+        lyric = lrc_json["lyric"]
+    except Exception as e:
+        print("Error when get music info in netease:", str(e))
+    return cover_url, play_url, lyric
+
+def try_search_in_kugou(music_name):
+    cover_url = ""
+    play_url  = ""
+    lyric     = ""
+
+    header = {"User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"}
+    music_search_url = "http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword={}&page=1&pagesize=10&showtype=1"
+    music_info_url = "http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash={}"
+    session = requests.Session()
+    try:
+        search_req = session.get(music_search_url.format(music_name+ " 银临"), headers=header, timeout=5)
+        search_data = search_req.json()
+        possible_hash = ""
+        for possible_music in search_data["data"]["info"]:
+            if "银临" in possible_music["singername"]:
+                possible_hash = possible_music["320hash"]
+                break
+        info_req = session.get(music_info_url.format(possible_hash), headers=header, timeout=5)
+        info_data = info_req.json()
+        cover_url = info_data["album_img"].replace("{size}", "400")
+        play_url = info_data["url"]
+    except Exception as e:
+        print("Error when get music info in kugou:", str(e))
+    return cover_url, play_url, lyric
+
 def get_music_detail(music_id):
     verify_object_id(music_id)
     try:
@@ -98,6 +143,10 @@ def get_music_detail(music_id):
     cover_url = ""
     play_url  = ""
     lyric     = ""
+    if cursor["platform"]["netease"] != None: 
+        cover_url ,play_url, lyric = get_netease_detail(cursor["platform"]["netease"])
+    else:
+        cover_url ,play_url, lyric = try_search_in_kugou(cursor["name"])
     return MusicDetailModel(
         id          = str(cursor["_id"]), 
         cover_url   = cover_url, 
