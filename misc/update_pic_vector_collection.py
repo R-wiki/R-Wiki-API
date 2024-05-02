2ff97058e7cbc896b1e6a3c93d5d69a641dc2e9c
nathan21hz <nathan21hz@gmail.com>
2024-05-02 15:59:56 +0800

    Update pic vector script

---
+++ misc/update_pic_vector_collection.py
@@ -0,0 +1,69 @@
+import sys
+import json
+import bson
+from datetime import datetime
+sys.path.append("..") 
+
+from config.general import load_config
+
+load_config("../config.ini")
+
+from config.db import db
+# from api.pic import get_latest_pic_list
+
+# pics ,_ = get_latest_pic_list(1,200)
+# 
+# for pic in pics:
+#     for pic_dir in pic.pics:
+#         print(pic_dir)
+#         db.pic_vector.insert_one({
+#             "set_id": bson.ObjectId(pic.id),
+#             "path": pic_dir
+#         })
+
+with open("update_pic_data.json", "r") as f:
+    data = json.loads(f.read())
+
+pic_type_map = {
+    "life": "生活照",
+    "live": "现场",
+    "artistic": "写真",
+    "screenshot":"截图",
+    "others":"其他"
+}
+
+for i in data:
+    print(i)
+    pic_info_arr = i.split("/")
+    pic_type = pic_type_map.get(pic_info_arr[1], "其他")
+    album_name = pic_info_arr[2]
+    file_path = pic_info_arr[3]
+    print(pic_type, album_name, file_path)
+    update_album = db.pic.find_one(
+        {"type": pic_type, "name": album_name}
+    )
+    if update_album:
+        db.pic.update_one(
+            {"type": pic_type, "name": album_name},
+            {"$addToSet":{"pics":i}}
+        )
+    else:
+        db.pic.insert_one({
+            "type"  : pic_type,
+            "name"  : album_name,
+            "date"  : datetime.now(),
+            "pics"  : [ i ],
+            "cover" : i,
+            "note"  : "",
+            "show"  : True
+        })
+        update_album = db.pic.find_one(
+            {"type": pic_type, "name": album_name}
+        )
+    # Update Vector Collection
+    print(update_album["_id"])
+    db.pic_vector.insert_one({
+        "set_id": update_album["_id"],
+        "path"  : i,
+        "vector": data[i]
+    })
