312a4b9f1789a1098dbaec6f19f7cc0c080cd1f4
nathan21hz <nathan21hz@gmail.com>
2024-07-18 22:15:47 +0800

    Use OpenAI API to load lyric lines to db

--- misc/update_lyric.py
+++ misc/update_lyric.py
@@ -4,7 +4,9 @@ import re
 import json
 from datetime import datetime
 from bson import ObjectId
-sys.path.append("..") 
+import requests
+from openai import OpenAI
+sys.path.append("..")
 
 from config.general import load_config
 
@@ -12,40 +14,55 @@ load_config("../config.ini")
 
 from config.db import db
 
-def findAllFile(base):
-    for root, ds, fs in os.walk(base):
-        for f in fs:
-            fullname = os.path.join(root, f)
-            yield fullname, f
+def lyric_dumper(lyric):
+    print("Loading from OpenAI...")
+    client = OpenAI()
+    completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "你是一个lrc文件处理工具，将收到的lrc字符串做处理为JSON格式数据，要求如下：1. 按照\\n分行；2. 去掉和歌词正文无关的行，如作词作曲信息，演奏者信息等，对唱提示等；3. 提取每行的时间标记和正文。时间标记需要删除方括号，如果有多个时间标记则解析为多行歌词，只需删除每句最后的标点符号。歌词正文需要删除歌手名或者男女对唱提示；4. 以JSON格式返回。格式为：{\"lyric_lines\":[{\"time_mark\":\"时间标记\",\"text\":\"正文\"}, ...]}，每句歌词解析为一个object，必须且只须要time_mark和text这两个key。",
+            },
+            {
+                "role": "user",
+                "content": lyric,
+            }
+        ],
+        model="gpt-4o",
+        response_format={ "type": "json_object" }
+    )
+    return json.loads(completion.choices[0].message.content)["lyric_lines"]
 
-for full_name, file_name in findAllFile(".\\lyrics"):
-    f = open(full_name, "r", encoding="utf-8")
-    lrc_lines = f.readlines()
-    print(file_name)
-    file_id = file_name.split("_")[0]
-    insert_list = []
-    for line in lrc_lines:
-        line = line.strip()
-        if line == "":
-            continue
-        match_obj = re.match(r"^(\[[0-9\.:]+\])+(.*?)$", line)
-        if not match_obj:
-            continue
-        match_items = match_obj.groups()
-        time_marks = [i.strip("[]") for i in match_items[:-1]]
-        text = match_items[-1]
-        print(time_marks, text)
-        if not text:
-            print("skip")
-            continue
-        else:
-            print("OK")
-            for tm in time_marks:
+music_list_req = requests.get("https://api.yinlin.wiki/music/latest?size=50")
+music_list = music_list_req.json()["data"]
+for music in music_list[:]:
+    existing_lyric = db.lyric.find_one({"music_id":ObjectId(music["id"])})
+    if existing_lyric:
+        print("Data exists, Pass.")
+        continue
+    music_info_req = requests.get("https://api.yinlin.wiki/music/detail?music_id="+music["id"])
+    music_info = music_info_req.json()["data"]
+    lyric_text = music_info["lyric"]
+    print(lyric_text)
+    if not lyric_text:
+        print("No lyric, Pass.")
+        continue
+    while True:
+        try:
+            lyric_lines = lyric_dumper(lyric_text)
+            insert_list = []
+            for lyric_line in lyric_lines:
+                print(lyric_line)
                 insert_list.append({
-                    "music_id"  : ObjectId(file_id),
-                    "time_mark" : tm,
-                    "text"      : text
+                    "music_id"  : ObjectId(music["id"]),
+                    "time_mark" : lyric_line["time_mark"],
+                    "text"      : lyric_line["text"]
                 })
-    print(insert_list)
-    if insert_list:
+            break
+        except:
+            print("----------Error retry----------")
+    # print(insert_list)
+    if insert_list and input() == "y":
         db.lyric.insert_many(insert_list)
+    else:
+        continue
